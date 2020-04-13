b503eed2c21f4ecbedf1728f07495d104cffdf30
tombom66 <46984443+tombom66@users.noreply.github.com>
2020-04-13 15:17:28 -0700

    Create utlist.h

---
+++ utlist.h
@@ -0,0 +1,16 @@
+//  utila.h
+//  linked_list
+//
+//  Created by William McCarthy on 094//20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+//
+
+#ifndef utila_h
+#define utila_h
+
+#include <stdbool.h>
+
+const char* yesorno(bool condition);
+
+
+#endif /* utila_h */
